9124a046f09f9a1b4a6b0cc04f13ebd8d24c1d7d
erikamaidana1950 <erikamaidana50@gmail.com>
2024-08-25 19:24:06 -0300

    primera funcion

--- js/products.js
+++ js/products.js
@@ -1,3 +1,13 @@
+const autos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+getJSONData = function(url) {
+    let result = {};
+return fetch(url)
+      .then(response => response.ok ? response.json() : Promise.reject(Error(response.statusText)))
+      .then(response => ({ status: 'ok', data: response }))
+      .catch(error => ({ status: 'error', data: error }));
+  }
+
 document.addEventListener("DOMContentLoaded", function() {
     getJSONData(autos).then(function(respObj) {
         if (respObj.status == "ok") {
