6b6c6c2788baceb575769005538fb16cd5383477
FaCami <camilafarroyo16842@hotmail.com>
2024-08-25 19:12:58 -0300

    Update login.js

---
+++ js/login.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username.trim() === '' || password.trim() === '') {
+        alert('Por favor, ingresa tu usuario y contraseña.');
+    } else {
+       
+        localStorage.setItem('loggedInUser', username);
+      
+        window.location.href = 'index.html';
+    }
+});
